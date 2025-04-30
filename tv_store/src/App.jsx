d7ecd09241e238635f8c2f12fd2583f2a02648b0
HoVanSang22636671 <HoVanSang22636671@users.noreply.github.com>
2025-04-30 19:22:47 +0700

    megre lan 1

--- tv_store/src/App.jsx
+++ tv_store/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-
+import Header from './components/Header'
 import Footer from "./components/Footer"
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +9,7 @@ function App() {
     <>
 
     {/*  */}
+    <Header/>
       <Footer/>
  
     </>
