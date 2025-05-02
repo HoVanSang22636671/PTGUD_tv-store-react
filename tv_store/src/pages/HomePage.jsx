cff95112574cd2c8f842f1ebe82f7fba711ea128
SANG <vansang12cb5@gmail.com>
2025-05-02 21:38:29 +0700

    9. Gia Ca Filter

--- tv_store/src/pages/HomePage.jsx
+++ tv_store/src/pages/HomePage.jsx
@@ -10,6 +10,7 @@ import ProductSuggest from "../components/ProductFilter/ProductSuggest";
 import Footer from "../components/Footer";
 
 import ThuongHieuFilter from "../components/FilterDetail/ThuongHieuFilter";
+import GiaCaFilter from "../components/FilterDetail/GiaCaFilter";
 import FilterProduct from "../components/FilterProduct";
 import Product from "../components/Product";
 import { useProduct } from '../API/UseProvider';
@@ -17,6 +18,7 @@ import { useProduct } from '../API/UseProvider';
 function HomePage() {
     const [filter, setFilter] = useState(1); // Bộ lọc hiện tại (1: Tất cả, 2: Thương hiệu, ...)
     const [thuongHieu, setThuongHieu] = useState([]); // Danh sách thương hiệu đã chọn
+    const [giaCa, setGiaCa] = useState(0);
     const [productList, setProductList] = useState([]); // Danh sách sản phẩm
     const { product } = useProduct(); // Lấy danh sách sản phẩm từ API/Provider
 
@@ -38,6 +40,15 @@ function HomePage() {
     useEffect(() => {
         setProductList(product);
     }, [product]);
+
+    let kq;
+    if (giaCa === "Giá cao đến thấp") {
+        kq = [...filterProduct].sort((a, b) => b.price - a.price);
+    } else if (giaCa === "Giá thấp đến cao") {
+        kq = [...filterProduct].sort((a, b) => a.price - b.price);
+    } else {
+        kq = [...filterProduct];
+    }
     return (
         <div className="bg-[#f5f5fa] transform transition-all duration-500">
             <div className="container mx-auto px-4">
@@ -57,16 +68,18 @@ function HomePage() {
                             setSelectedBrands={setThuongHieu}
                         />
                     )}
+
+                    {/* Lọc theo giá cả */}
+                    {filter === 3 && (
+                        <GiaCaFilter giaCa={giaCa} setGiaCa={setGiaCa} />
+                    )}
                     {/* Sản phẩm được lọc */}
-                    {thuongHieu.length > 0 && (
+                    {/* Sản phẩm được lọc */}
+                    {(thuongHieu.length > 0 || giaCa !== 0) && kq.length > 0 && (
                         <FilterProduct title={"Kết quả lọc"}>
-                            {productList
-                                .filter((item) =>
-                                    thuongHieu.length === 0 || thuongHieu.includes(item.thuongHieu)
-                                )
-                                .map((product) => (
-                                    <Product key={product.id} product={product} />
-                                ))}
+                            {kq.map((product) => (
+                                <Product key={product.id} product={product} />
+                            ))}
                         </FilterProduct>
                     )}
                     {/* Sản phẩm sale */}
